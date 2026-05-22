987d37ec34702a9e8db1b0d05fe82045f9b2757e
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-22 12:21:12 -0600

    Update user configuration.py

--- user configuration.py
+++ user configuration.py
@@ -1,48 +1,48 @@
-test_settings =[
-    {
+test_settings = {
         'theme':'dark',
-        'languages':'english'
-    },
-    {
-        'theme':'ligth',
-        'languages':'spanish'
-    },
-    {
-        'theme':'blue',
-        'languages':'french'
-    }
+        'notifications':'disable',
+        'volume':'low',
+        'xd':'a'
+}
       
-]
-
 def add_setting(dictionary_set, tuple_set):
-    key, value = dictionary_set
+    key, value = tuple_set
     key = key.lower()
     value = value.lower()
-    
     if key in dictionary_set:
         return f"Setting '{key}' already exists! Cannot add a new setting with this name."
-    dictionary_set[key] = value
-    return f"Setting '{key}' added with value '{value}' successfully!."
+    else:
+        dictionary_set[key] = value
+        return f"Setting '{key}' added with value '{value}' successfully!"
 
-def update_setting(dictionary_set, tuple_set):
-    key, value = dictionary_set
+
+def update_setting(dictionary_sett, tuple_sett):
+    key, value = tuple_sett
     key = key.lower()
     value = value.lower()
-    if key in dictionary_set:
-        return f"Setting '{key}' update to {value} successfully."
-    return f"Setting '{key}' does not exist! Cannot update a non-existing setting."
+    if key in dictionary_sett:
+        dictionary_sett[key] = value
+        return f"Setting '{key}' updated to '{value}' successfully!"
+    else:
+        return f"Setting '{key}' does not exist! Cannot update a non-existing setting."
 
-def delate_setting(dictionary_set, tuple_set):
-    key, value = dictionary_set
+def delete_setting(dictionary_setti, tuple_setti):
+    key = tuple_setti
     key = key.lower()
-    if key in dictionary_set:
-        dictionary_set.pop(key)
-        return f"Setting '{key}' deleted successfully."
-    return f"Setting not found!"
+    if key in dictionary_setti:
+        dictionary_setti.pop(key)
+        return f"Setting '{key}' deleted successfully!"
+    else:
+        return f"Setting not found!"
 
-def view_setting(dictionary_set):
-    if dictionary_set == "":
+def view_settings(dictionary_setting):
+    if not dictionary_setting:
         return 'No settings available.'
     else:
-        lines = [f"{key.capitalize()}: {value}" for key, value in dictionary_set.items()]
-        return "Current User Settings:\n" + "\n".join(lines)
+        result = "Current User Settings:\n"
+        for key, value in dictionary_setting.items():
+            result += f"{key.capitalize()}: {value}\n"
+
+        return result
+
+print (update_setting(test_settings,('theme','dark')))
