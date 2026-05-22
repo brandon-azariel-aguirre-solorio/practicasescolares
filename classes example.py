f5ca867bfd60e972b5553c88b2277d4c4ab9b394
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-22 17:23:37 -0600

    Update classes example.py

--- classes example.py
+++ classes example.py
@@ -25,3 +25,20 @@ class Cart:
 
    def __iter__(self):
        return iter(self.items)
+class Person: 
+    def __init__(self, name, age): 
+        self.name = name 
+        self.age = age 
+
+person = Person('John Doe', 30)
+
+# Loop through all attributes of the person object with dir() function
+for attr in dir(person):
+    # Ignore dunder methods like __init__ or __str__ and regular methods
+    if not attr.startswith('__') and not callable(getattr(person, attr)): 
+        value = getattr(person, attr)
+        print(f'{attr}: {value}')
+
+# Output
+# age: 30
+# name: John Doe
