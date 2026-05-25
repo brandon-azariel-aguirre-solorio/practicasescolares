0ec413a1364710bdae95b5c25a779b437a9d8e39
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-25 15:20:34 -0600

    Update salary_tracker_class.py

--- salary_tracker_class.py
+++ salary_tracker_class.py
@@ -1,14 +1,48 @@
 class Employee:
+    _base_salaries = {
+        'trainee': 1000,
+        'junior': 2000,
+        'mid-level': 3000,
+        'senior': 4000,
+    }
+
     def __init__(self, name, level):
-        self._name = name
-        self._level = level
+        if level not in Employee._base_salaries:
+            raise ValueError(f"Invalid value '{level}' for 'level' attribute.")
+        self.name = name
+        self.level = level
+        self._salary = Employee._base_salaries[level]
 
     def __str__(self):
-        return f'{self._name}: {self._level}'
+        return f'{self.name}: {self.level}'
+
+    def __repr__(self):
+        return f"Employee('{self.name}', '{self.level}')"
 
     @property
     def name(self):
         return self._name
 
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError("'name' must be a string.")
+        self._name = new_name
+        print(f"'name' updated to '{self.name}'.")
+
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, new_level):
+        
+        self._level = new_level
+
+    @property
+    def salary(self):
+        return self._salary
+
 charlie_brown = Employee('Charlie Brown', 'trainee')
 print(charlie_brown)
+print(f'Base salary: ${charlie_brown.salary}')
