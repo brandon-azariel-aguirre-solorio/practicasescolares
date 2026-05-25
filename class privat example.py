83b219a3814fbe549522c6c1d9a0010046797e43
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-25 10:49:50 -0600

    Update class privat example.py

--- class privat example.py
+++ class privat example.py
@@ -29,3 +29,20 @@ print(acct_one.get_balance()) # 53
 acct_one.deposit(-4)  # ValueError: Amount must be positive
 acct_one.withdraw(-8) # ValueError: Amount must be positive
 acct_one.withdraw(58) # ValueError: Insufficient funds
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self): # A getter to get the radius
+        return self._radius
+  
+    @property
+    def area(self):  # A getter to calculate area
+        return 3.14 * (self._radius ** 2)
+
+my_circle = Circle(3)
+
+print(my_circle.radius) # 3
+print(my_circle.area) # 28.26
