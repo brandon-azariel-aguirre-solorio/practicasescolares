a4241116f9526282d0955ba4ff24aa2b3a7061d1
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-26 10:22:33 -0600

    Update Discount calculator exercise.py

--- Discount calculator exercise.py
+++ Discount calculator exercise.py
@@ -1,6 +1,21 @@
+from abc import ABC, abstractmethod
+
 class Product:
-    def __init__(self, name: str, price: float):
+    def __init__(self, name: str, price: float) -> None:
         self.name = name
         self.price = price
-    def __str__(self):
+
+    def __str__(self) -> str:
         return f'{self.name} - ${self.price}'
+
+class DiscountStrategy(ABC):
+    @abstractmethod
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        pass
+
+    @abstractmethod
+    def apply_discount(self, product: Product) -> float:
+        pass
+
+product = Product('Wireless Mouse', 50.0)
+print(product)
