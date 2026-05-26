1d28dfb4e1fec6b4489a4b4cdf23c43e905ea915
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-26 10:50:17 -0600

    Update Discount calculator exercise.py

--- Discount calculator exercise.py
+++ Discount calculator exercise.py
@@ -17,5 +17,22 @@ class DiscountStrategy(ABC):
     def apply_discount(self, product: Product) -> float:
         pass
 
+class PercentageDiscount(DiscountStrategy):
+    def __init__(self, percent: int) -> None:
+        self.percent = percent
+
+    def is_applicable(self, product: Product, user_tier: str) -> bool:
+        return self.percent <= 70
+
+    def apply_discount(self, product: Product) -> float:
+        return product.price * (1 - self.percent / 100)
+
+class FixedAmountDiscount(DiscountStrategy):
+    def __init__(self, amount: int) -> None:
+        self.amount = amount
+    
 product = Product('Wireless Mouse', 50.0)
 print(product)
+
+discount = PercentageDiscount(10)
+print(discount.apply_discount(product))
