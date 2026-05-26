308728ac6bb591129644d3858671ddfe1626ffa1
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-25 19:19:51 -0600

    Update media cataloge exersise.py

--- media cataloge exersise.py
+++ media cataloge exersise.py
@@ -16,18 +16,44 @@ class Movie:
     def __str__(self):
         return f'{self.title} ({self.year}) - {self.duration} min, {self.director}'
 
+class TVSeries(Movie):
+    def __init__(self, title, year, director, duration, seasons, total_episodes):
+        super().__init__(title, year, director, duration)
+
+        if seasons < 1:
+            raise ValueError('Seasons must be 1 or greater')
+        if total_episodes < 1:
+            raise ValueError('Total episodes must be 1 or greater')
+        
+        self.seasons = seasons
+        self.total_episodes = total_episodes
+
+
 class MediaCatalogue:
     def __init__(self):
         self.items = []
 
     def add(self, media_item):
         self.items.append(media_item)
+
     def __str__(self):
         if not self.items:
             return 'Media Catalogue (empty)'
+
         result = f'Media Catalogue ({len(self.items)} items):\n\n'
         
+        for i, movie in enumerate(self.items, 1):
+            result += f'{i}. {movie}\n'
+        return result
+
+catalogue = MediaCatalogue()
 try:
     movie1 = Movie('The Matrix', 1999, 'The Wachowskis', 136)
+    catalogue.add(movie1)
+    movie2 = Movie('Inception', 2010, 'Christopher Nolan', 148)
+    catalogue.add(movie2)
+    
+
+    print(catalogue)
 except ValueError as e:
     print(f'Validation Error: {e}')
