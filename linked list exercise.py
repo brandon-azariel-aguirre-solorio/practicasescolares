77b9b40ecebe0d9ed6f3ac40a18ef038f2e58009
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-26 17:12:06 -0600

    Update and rename linked list exercise to linked list exercise.py

---
+++ linked list exercise.py
@@ -0,0 +1,24 @@
+class LinkedList:
+    class Node:
+        def __init__(self, element):
+            self.element = element
+            self.next = None
+            
+    def __init__(self):
+        self.length = 0
+        self.head = None
+
+    def is_empty(self):
+        return self.length == 0
+    
+    def add(self, element):
+        node = self.Node(element)
+        if self.is_empty():
+            self.head = node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+
+my_list = LinkedList()
+print(my_list.is_empty())
