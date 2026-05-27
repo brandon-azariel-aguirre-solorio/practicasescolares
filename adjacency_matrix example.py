f4f2bedcfb0357e278461faa1c2cbc25c6188b62
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-27 17:48:12 -0600

    Create adjacency_matrix example.py

---
+++ adjacency_matrix example.py
@@ -0,0 +1,6 @@
+adjacency_matrix = [
+    [0, 1, 1, 1],  # The neighbors of A are B, C, and D
+    [1, 0, 0, 1],  # The neighbors of B are A and D
+    [1, 0, 0, 0],  # The only neighbor of C is A
+    [1, 1, 0, 0]   # The neighbors of D are A and B
+]
