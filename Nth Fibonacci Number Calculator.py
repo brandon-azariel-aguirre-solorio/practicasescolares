8bbf672b0461f4c1ea12d0ebf2b246075c4894ef
brandon <54422488+brandon-azariel-aguirre-solorio@users.noreply.github.com>
2026-05-28 09:26:15 -0600

    Create Nth Fibonacci Number Calculator.py

---
+++ Nth Fibonacci Number Calculator.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    sequence = [0, 1]
+
+    if n < 0:
+        return sequence[0]
+    elif n == 1:
+        return sequence[1]
+
+    for i in range(2, n + 1):
+        sequence.append(sequence[i - 1] + sequence[i - 2])
+
+    return sequence[n]
+
+print(fibonacci(1))Nth Fibonacci Number Calculator
